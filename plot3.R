57fbce66c96a64326aceb6de0ff2cebdf261fe62
Bob <info@bobophof.nl>
2016-09-08 15:58:18 +0200

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -13,15 +13,14 @@ download.file(fileUrl,destfile="./Exploratory/Dataset.zip")
 unzip(zipfile="./Exploratory/Dataset.zip",exdir="./Exploratory")
 baseSet <- read.table("./Exploratory/household_power_consumption.txt", header = TRUE, sep = ";", na.strings="?", stringsAsFactors=FALSE, dec=".")
 
-### Creating the subset with focus days
+### Creating the subset with focus days and preparation for plotting
 subbaseSet <- baseSet[baseSet$Date %in% c("1/2/2007","2/2/2007") ,]
 globalActivePower <- as.numeric(subbaseSet$Global_active_power)
-
-### Plot
 subMetering1 <- as.numeric(subbaseSet$Sub_metering_1)
 subMetering2 <- as.numeric(subbaseSet$Sub_metering_2)
 subMetering3 <- as.numeric(subbaseSet$Sub_metering_3)
 
+### Plot
 png("./Exploratory/plot3.png", width=480, height=480)
 plot(datetime, subMetering1, type="l", ylab="Energy Submetering", xlab="")
 lines(datetime, subMetering2, type="l", col="red")
